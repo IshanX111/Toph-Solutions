bb753f68c77916bd380f082be5fdd4578fc9019b
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2022-07-29 21:07:17 +0600

    Add files via upload

---
+++ reverse-man.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     string s;
+     getline(cin,s);
+
+     reverse(s.begin(),s.end());
+     cout<<s<<endl;
+
+
+}
