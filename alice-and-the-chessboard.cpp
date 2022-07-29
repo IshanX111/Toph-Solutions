b6389c54fcbaaedb1654f0c6d73023c3403574f4
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2022-07-29 20:46:10 +0600

    Add files via upload

---
+++ alice-and-the-chessboard.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+            ll a,b,c,d;
+            cin>>a>>b>>c>>d;
+            if((abs(a-c)==abs(b-d))){
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
+
+
+
+    }
+
+
+
+
+
+
+
+}
